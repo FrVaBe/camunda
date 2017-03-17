69d120b76091dc25d6ac52cb094ec4279cd0c3ff
Franz van Betteraey <franz.van.betteraey@heuboe.de>
2017-03-17 16:52:17 +0100

    add camunda-spring-boot-starter-unit-testing-demo

---
+++ camunda-spring-boot-starter-unit-testing-demo/src/test/java/de/frvabe/bpm/camunda/ProductionProcessTest.java
@@ -0,0 +1,33 @@
+package de.frvabe.bpm.camunda;
+
+import static org.camunda.bpm.engine.test.assertions.ProcessEngineAssertions.assertThat;
+
+import org.camunda.bpm.engine.RuntimeService;
+import org.camunda.bpm.engine.TaskService;
+import org.camunda.bpm.engine.runtime.ProcessInstance;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = {Main.class})
+public class ProductionProcessTest {
+
+    private static final String PRODUCTION_PROCESS_KEY = "productionDemoProcess";
+
+    @Autowired
+    RuntimeService runtimeService;
+
+    @Autowired
+    TaskService taskService;
+
+    @Test
+    public void startProductionProcess() throws InterruptedException {
+        ProcessInstance pi = runtimeService.startProcessInstanceByKey(PRODUCTION_PROCESS_KEY);
+        assertThat(pi).isNotNull();
+        runtimeService.deleteProcessInstance(pi.getId(), "JUnit test");
+    }
+
+}
---
+++ camunda-spring-boot-starter-unit-testing-demo/src/test/java/de/frvabe/bpm/camunda/TestProcessTest.java
@@ -0,0 +1,33 @@
+package de.frvabe.bpm.camunda;
+
+import static org.camunda.bpm.engine.test.assertions.ProcessEngineAssertions.assertThat;
+
+import org.camunda.bpm.engine.RuntimeService;
+import org.camunda.bpm.engine.TaskService;
+import org.camunda.bpm.engine.runtime.ProcessInstance;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = {Main.class})
+public class TestProcessTest {
+
+    private static final String TEST_PROCESS_KEY = "testDemoProcess";
+
+    @Autowired
+    RuntimeService runtimeService;
+
+    @Autowired
+    TaskService taskService;
+
+    @Test
+    public void startTestProcess() throws InterruptedException {
+        ProcessInstance pi = runtimeService.startProcessInstanceByKey(TEST_PROCESS_KEY);
+        assertThat(pi).isNotNull();
+        runtimeService.deleteProcessInstance(pi.getId(), "JUnit test");
+    }
+
+}
